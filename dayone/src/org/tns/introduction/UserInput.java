8fcc890d9be280829339280a1689271e57e38332
prati <prati@Pratik>
2023-07-20 14:07:18 +0530

    first commit

---
+++ dayone/src/org/tns/introduction/UserInput.java
@@ -0,0 +1,16 @@
+//program to demonstrate on user-input value
+package org.tns.introduction;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+	Scanner s= new Scanner(System.in);
+	System.out.println("enter a string:");
+	String a=s.nextLine();
+	System.out.println(" string is "+a);
+
+	}
+
+}
